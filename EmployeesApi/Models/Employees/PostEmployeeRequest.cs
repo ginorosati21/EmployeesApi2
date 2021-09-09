172e28fc979c7ef75c0e6a35a78588f998de3326
ginorosati21 <ginorosati21@gmail.com>
2021-09-09 15:13:21 -0400

    Initial commit

---
+++ EmployeesApi/Models/Employees/PostEmployeeRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesApi.Models.Employees
+{
+    public class PostEmployeeRequest
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+    }
+
+}
